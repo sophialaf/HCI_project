b29f5590f2bdd5a3d286978b9c22de1b260c0eeb
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-11-15 12:41:40 +0100

    connected to db, no fetch though

--- hci/express.js
+++ hci/express.js
@@ -1,29 +1,28 @@
 const express = require('express');
-// const db = require('./db');
 const app = express();
+const port = 3000;
 const path = require('path');
-const PORT = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 
-// Serve the home page
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-// Serve static files (html, css, js, images, etc.)
 app.get(['/*.html', '/*.css', '/*.js', '/*.jpg', '/*.jpeg'], (req, res) => {
     res.sendFile(path.join(__dirname, req.path));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+// Use the default flag OPEN_CREATE, which creates the database file if it doesn't exist
+const db = new sqlite3.Database('./hci_database.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
 });
 
-app.get('/recipes', (req, res) => {
-    db.all('SELECT * FROM Recipes', (err, rows) => {
-        if (err) {
-            res.status(500).send(err.message);
-            return;
-        }
-        res.json(rows);
-    });
+let server = app.listen(port, function () {
+    console.log("App server is running on port", port);
+    console.log("To end, press Ctrl + C");
 });
