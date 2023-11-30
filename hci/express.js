99f4d5ef4ecd6d4b95576d5b13765e83d4133d33
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-11-30 21:10:05 +0100

    combined working db connect and working buttons

--- hci/express.js
+++ hci/express.js
@@ -12,7 +12,6 @@ app.get(['/*.html', '/*.css', '/*.js', '/*.jpg', '/*.jpeg'], (req, res) => {
     res.sendFile(path.join(__dirname, req.path));
 });
 
-// Use the default flag OPEN_CREATE, which creates the database file if it doesn't exist
 const db = new sqlite3.Database('./hci_database.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
@@ -21,7 +20,6 @@ const db = new sqlite3.Database('./hci_database.db', sqlite3.OPEN_CREATE | sqlit
     }
 });
 
-// Serve the main HTML page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
@@ -43,8 +41,61 @@ app.get('/getAllRecipes', (req, res) => {
             console.error(err.message);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
-            console.log('All recipes:', rows);
+            const numberOfRecipes = rows.length;
+            console.log(`all Selected ${numberOfRecipes} recipes:`);
             res.json({ recipes: rows });
         }
     });
+});
+
+
+// Handle POST request to get recipes without selected allergens
+app.post('/getRecipes', (req, res) => {
+    const selectedAllergens = req.body.allergens;
+
+    if (!selectedAllergens || selectedAllergens.length === 0) {
+        // If no allergens selected, return all recipes
+        const query = `
+            SELECT *
+            FROM recipes
+        `;
+
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                const numberOfRecipes = rows.length;
+            console.log(`Selected ${numberOfRecipes} recipes:`);
+                res.json({ recipes: rows });
+            }
+        });
+    } else {
+        // If allergens selected, return recipes without those allergens
+        const allergenNames = selectedAllergens.map(name => `'${name}'`).join(',');
+
+        const query = `
+            SELECT DISTINCT recipes.*
+            FROM recipes
+            WHERE id NOT IN (
+                SELECT DISTINCT recipes.id
+                FROM recipes
+                JOIN allergens ON recipes.allergens LIKE '%' || allergens.allergen_name || '%'
+                WHERE allergens.allergen_name IN (${allergenNames})
+            )
+        `;
+
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                const numberOfRecipes = rows.length;
+                console.log(`idk which this is either Selected ${numberOfRecipes} recipes:`);
+                res.json({ recipes: rows });
+                // console.log('Filtered recipes:', rows);
+                res.json({ recipes: rows });
+            }
+        });
+    }
 });
