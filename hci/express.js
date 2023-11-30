ac895a329188a4609a6d7051d72ad29418e111f4
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-11-30 17:51:19 +0100

    work on the selected allergens, not yet working

--- hci/express.js
+++ hci/express.js
@@ -8,36 +8,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.route('/')
-    .get((req, res) => {
-        res.sendFile(path.join(__dirname, 'home.html'));
-    })
-    .post((req, res) => {
-        const selectedAllergens = req.body.allergens;
-
-        // Create a comma-separated string for the selected allergens to use in the SQL query
-        const allergenNames = selectedAllergens.map(name => `'${name}'`).join(',');
-
-        // Example query to retrieve recipes without selected allergens
-        const query = `
-            SELECT DISTINCT recipes.*
-            FROM recipes
-            LEFT JOIN allergens_recipes ON recipes.recipe_id = allergens_recipes.recipe_id
-            LEFT JOIN allergens ON allergens_recipes.allergen_id = allergens.allergen_id
-            WHERE allergens.name IS NULL OR allergens.name NOT IN (${allergenNames})
-        `;
-        console.log('SQL Query:', query); // Log the SQL query
-        db.all(query, (err, rows) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).json({ error: 'Internal Server Error' });
-            } else {
-                console.log('Result from the database:', rows);
-                res.json({ recipes: rows });
-            }
-        });
-    });
-
 app.get(['/*.html', '/*.css', '/*.js', '/*.jpg', '/*.jpeg'], (req, res) => {
     res.sendFile(path.join(__dirname, req.path));
 });
@@ -61,9 +31,6 @@ let server = app.listen(port, function () {
     console.log("To end, press Ctrl + C");
 });
 
-/*
-Testing all recipes
-*/
 // Handle GET request to get all recipes
 app.get('/getAllRecipes', (req, res) => {
     const query = `
